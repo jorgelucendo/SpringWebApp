880ee9a70014ec741c833beb4392f33ee9853e95
Jorge Lucendo Esteban <jorgelucendos@gmail.com>
2022-03-08 09:47:52 +0100

    E17

---
+++ src/main/java/com/example/SpringWebApp/bootstrap/BoostStrapData.java
@@ -0,0 +1,43 @@
+package com.example.SpringWebApp.bootstrap;
+
+import com.example.SpringWebApp.domain.Author;
+import com.example.SpringWebApp.domain.Book;
+import com.example.SpringWebApp.repositories.AuthorRepository;
+import com.example.SpringWebApp.repositories.BookRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BoostStrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
+    public BoostStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Design", "123123");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+        Author rod = new Author("Rod","Jhonson");
+        Book noEJB = new Book("J2EE Development without EJB","3939459459");
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+
+        System.out.println("Started in Boostrap");
+        System.out.println("Number of books: "+bookRepository.count());
+    }
+}
