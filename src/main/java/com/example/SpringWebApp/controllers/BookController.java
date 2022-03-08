0d90a851ffdb99c66e1a365d366a93f42467005f
Jorge Lucendo Esteban <jorgelucendos@gmail.com>
2022-03-08 12:06:13 +0100

    E22
    Add and test thymeleaf

--- src/main/java/com/example/SpringWebApp/controllers/BookController.java
+++ src/main/java/com/example/SpringWebApp/controllers/BookController.java
@@ -1,2 +1,24 @@
-package com.example.SpringWebApp.controllers;public class BookController {
+package com.example.SpringWebApp.controllers;
+
+import com.example.SpringWebApp.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+
+        model.addAttribute("books",bookRepository.findAll());
+
+        return "books";
+    }
 }
